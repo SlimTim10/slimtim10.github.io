c05d7550b8d91f69b61e9f35a1beb6b55322d437
SlimTim10 <slimtim10@gmail.com>
2017-01-11 17:38:22 -0500

    Refactor formatting

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	var setInnerHTML = function(elementId, html) {
-		document.getElementById(elementId).innerHTML = html;
-	};
-	
-	var setContent = function(contentId, contentFile) {
-		var HTTP_OK = 200;
-		var httpRequest = new XMLHttpRequest();
+  var setInnerHTML = function(elementId, html) {
+    document.getElementById(elementId).innerHTML = html;
+  };
+  
+  var setContent = function(contentId, contentFile) {
+    var HTTP_OK = 200;
+    var httpRequest = new XMLHttpRequest();
 
-		if (!httpRequest) {
-			return false;
-		}
+    if (!httpRequest) {
+      return false;
+    }
 
-		httpRequest.onreadystatechange = function() {
-			if (httpRequest.readyState === XMLHttpRequest.DONE) {
-				if (httpRequest.status === HTTP_OK) {
-					setInnerHTML(contentId, httpRequest.responseText);
-				} else {
-					return false;
-				}
-			}
-		};
-		httpRequest.open("GET", contentFile);
-		httpRequest.send();
-	};
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === HTTP_OK) {
+          setInnerHTML(contentId, httpRequest.responseText);
+        } else {
+          return false;
+        }
+      }
+    };
+    httpRequest.open("GET", contentFile);
+    httpRequest.send();
+  };
 
-	var setupContentLink = function(contentId, linkId, contentFile) {
-		document.getElementById(linkId).addEventListener("click", function(e) {
-			setContent(contentId, contentFile);
-		});
-	};
+  var setupContentLink = function(contentId, linkId, contentFile) {
+    document.getElementById(linkId).addEventListener("click", function(e) {
+      setContent(contentId, contentFile);
+    });
+  };
 
-	var setupContentLinks = function(links) {
-		links.forEach(function(link) {
-			setupContentLink(link[0], link[1], link[2]);
-		});
-	};
+  var setupContentLinks = function(links) {
+    links.forEach(function(link) {
+      setupContentLink(link[0], link[1], link[2]);
+    });
+  };
 
-	setupContentLinks([
-		["content", "about-link", "about.html"],
-		["content", "projects-link", "projects.html"],
-		["content", "blog-link", "blog.html"],
-	]);
+  setupContentLinks([
+    ["content", "about-link", "about.html"],
+    ["content", "projects-link", "projects.html"],
+    ["content", "blog-link", "blog.html"],
+  ]);
 }());
