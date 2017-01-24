363932ddd0a10cc7d3d0b343572c9c7dcfb717f1
SlimTim10 <slimtim10@gmail.com>
2017-01-24 17:44:20 -0500

    Add category page for displaying projects

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 (function() {
   "use strict";
 
+  var fillHead = function() {
+    document.head.innerHTML += '<link href="https://fonts.googleapis.com/css?family=Raleway:700|Source+Sans+Pro|PT+Sans+Narrow:700" rel="stylesheet">';
+  };
+
   var setInnerHTML = function(elementId, html) {
     var el = document.getElementById(elementId);
     if (el) {
@@ -29,7 +33,22 @@
     httpRequest.send();
   };
 
+  var pageLoadContent = function() {
+    var hash = window.location.hash.split(":");
+    var id = hash[0].slice(1);
+    var page = hash[1];
+    if (id && page) {
+      setContent(id, page + ".html");
+    }
+  };
+
+  fillHead();
+  
   setContent("footer", "footer.html");
   setContent("banner", "banner.html");
   setContent("categories", "categories.html");
+
+  pageLoadContent();
+
+  window.onhashchange = pageLoadContent;
 }());
