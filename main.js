899747a396654548a1b3cbf57c1b8be032f0fd1c
SlimTim10 <slimtim10@gmail.com>
2017-01-23 20:17:55 -0500

    Load header and footer with javascript

--- main.js
+++ main.js
@@ -26,34 +26,6 @@
     httpRequest.send();
   };
 
-  var setupContentLink = function(contentId, linkId, contentFile) {
-    document.getElementById(linkId).addEventListener("click", function(e) {
-      setContent(contentId, contentFile);
-    });
-  };
-
-  var setupContentLinks = function(links) {
-    links.forEach(function(link) {
-      setupContentLink(link[0], link[1], link[3]);
-    });
-  };
-
-  // Load correct content on page load based on hash link
-  var pageLoadContent = function(links) {
-    links.forEach(function(link) {
-      if (window.location.hash === link[2]) {
-        setContent(link[0], link[3]);
-      }
-    });
-  };
-
-  var links = [
-    ["content", "about-link", "#about", "about.html"],
-    ["content", "projects-link", "#projects", "projects.html"],
-    ["content", "blog-link", "#blog", "blog.html"],
-  ];
-  
-  setupContentLinks(links);
-  setContent(links[0][0], links[0][3]); // Default content
-  pageLoadContent(links);
+  setContent("footer", "footer.html");
+  setContent("header", "header.html");
 }());
