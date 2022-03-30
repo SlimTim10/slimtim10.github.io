3d5c2e55cdeb935495dc4a2412a207c4e7d8dc10
SlimTim10 <slimtim10@gmail.com>
2022-03-30 17:43:33 -0400

    Nav menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
+const mobileMenu = () => {
+  const openButton = document.querySelector('header #toggle-menu-open')
+  const closeButton = document.querySelector('header #toggle-menu-close')
+  const menu = document.querySelector('header nav')
+  menu.classList.add('collapsed')
+  openButton.onclick = () => {
+    menu.classList.remove('collapsed')
+    openButton.classList.add('hidden')
+    closeButton.classList.remove('hidden')
+  }
+  closeButton.onclick = () => {
+    menu.classList.add('collapsed')
+    openButton.classList.remove('hidden')
+    closeButton.classList.add('hidden')
+  }
+}
+
+mobileMenu()
+
 const collapsibleCurriculum = () => {
   const sections = document.querySelectorAll('section#curriculum > ul > li')
   sections.forEach(section => {
