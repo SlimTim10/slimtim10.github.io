e94ca8b4ceaa2bf59ea3f9c54ef1f18b972a0d77
SlimTim10 <slimtim10@gmail.com>
2022-03-30 17:54:46 -0400

    Nav menu button animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,10 @@
 const mobileMenu = () => {
-  const openButton = document.querySelector('header #toggle-menu-open')
-  const closeButton = document.querySelector('header #toggle-menu-close')
+  const button = document.querySelector('header #toggle-menu')
   const menu = document.querySelector('header nav')
   menu.classList.add('collapsed')
-  openButton.onclick = () => {
-    menu.classList.remove('collapsed')
-    openButton.classList.add('hidden')
-    closeButton.classList.remove('hidden')
-  }
-  closeButton.onclick = () => {
-    menu.classList.add('collapsed')
-    openButton.classList.remove('hidden')
-    closeButton.classList.add('hidden')
+  button.onclick = () => {
+    menu.classList.toggle('collapsed')
+    button.classList.toggle('opened')
   }
 }
 
